8cecda5ae843c4999f351009ae3ade43b3777f5c
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-14 14:12:40 +0000

    search book fucntion added

--- ebook_class.py
+++ ebook_class.py
@@ -13,3 +13,14 @@ class Ebook(MSDBConnection):
                 break
             print(f"Title: {record.title} - Author: {record.author} - Date: {record.date_published}")
         print('All Records Retrieved')
+
+    def search_ebook(self):
+        ask_book = input('What is the name of your book?')
+        query = f"SELECT * FROM ebooks WHERE title LIKE '%{ask_book}%'"
+        result = self._MSDBConnection__sql_query(query)
+        while True:
+            record = result.fetchone()
+            if record is None:
+                break
+            print(record)
+        return 'All Books Listed'
