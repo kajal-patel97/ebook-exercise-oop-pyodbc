ac94595bac1142fb5625de381d9392e8572133d3
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-14 14:39:16 +0000

    function added to run file and new function to create a book was made and added to run file

--- ebook_class.py
+++ ebook_class.py
@@ -1,6 +1,7 @@
 import pyodbc
 from connect_sql import *
 
+
 class Ebook(MSDBConnection):
 
     def get_all_books(self):
@@ -16,11 +17,24 @@ class Ebook(MSDBConnection):
 
     def search_ebook(self):
         ask_book = input('What is the name of your book?')
-        query = f"SELECT * FROM ebooks WHERE title LIKE '%{ask_book}%'"
+        query = f"SELECT * FROM ebooks WHERE title LIKE '%{ask_book.title()}%'"
         result = self._MSDBConnection__sql_query(query)
         while True:
             record = result.fetchone()
             if record is None:
                 break
-            print(record)
-        return 'All Books Listed'
+            print(
+                f"Title: {record.title} - Author: {record.author} - Genre: {record.genre} - Date Published: {record.date_published} - Postcode: {record.postcode} - Longitude/Latitude: {record.longitude}/{record.latitude}")
+        print('All Books Listed')
+
+    def add_ebook(self):
+        ask_title = input('What is the title of the book you would like to add?')
+        ask_author = input('What is the Author of this book?')
+        ask_genre = input('What genre does this book come under?')
+        ask_date_year = input('What year was this published in? (please user numerical values)')
+        ask_date_month = input('What month was the book published (please user numerical values)')
+        ask_date_day = input('Finally what day was this book published? (please user numerical values)')
+        query = f"INSERT INTO ebooks (title, author, genre, date_published) VALUES ('{ask_title.title()}', '{ask_author.title()}', '{ask_genre.title()}', '{ask_date_year}-{ask_date_month}-{ask_date_day}')"
+        result = self._MSDBConnection__sql_query(query)
+        self.docker_ebook_store.commit()
+        return result
--- store_run.py
+++ store_run.py
@@ -6,14 +6,23 @@ table_ebooks = Ebook()
 while True:
     print('_________________________________________')
     print('Option 1 -- Get all Books')
-
-
-
+    print('Option 2 -- Search for a book')
+    print('Option 3 -- Add a Book')
 
     print('_________________________________________')
+
+
     user_input = input('Please choose a number from the options listed...')
     print('_________________________________________')
 
     if user_input == '1':
         print('Getting all Books...')
         table_ebooks.get_all_books()
+
+    elif user_input == '2':
+        table_ebooks.search_ebook()
+
+    elif user_input == '3':
+        print('We need some information to add a new book in!')
+        table_ebooks.add_ebook()
+        print('Book has been added!')
