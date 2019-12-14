1a455414b6e3e28c1cc79bf2834ad5c38ed0a6d3
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-14 13:50:52 +0000

    connection made

--- connect_sql.py
+++ connect_sql.py
@@ -1,1 +1,18 @@
-# Need to import 
+# Need to import pyodbc
+
+import pyodbc
+
+class MSDBConnection():
+
+    def __init__(self):
+        self.server = 'localhost,1433'
+        self.database = 'ebook_store'
+        self.user_name = 'SA'
+        self.password = 'Passw0rd2018'
+
+        self.docker_ebook_store = pyodbc.connect( 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.user_name + ';PWD=' + self.password)
+
+        self.cursor = self.docker_ebook_store.cursor()
+
+    def __sql_query(self,sql_query):
+        return self.cursor.execute(sql_query)
